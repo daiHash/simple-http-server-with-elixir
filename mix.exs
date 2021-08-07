fe976ae88fdba15c47e9d3c597af0e8084b59291
Daichi Hashimura <dhashi92@gmail.com>
2021-08-07 14:54:24 +0900

    Rename

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule SimpleHttpServerWithElixir.MixProject do
+defmodule HttpServer.MixProject do
   use Mix.Project
 
   def project do
--- test/simple_http_server_with_elixir_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule SimpleHttpServerWithElixirTest do
-  use ExUnit.Case
-  doctest SimpleHttpServerWithElixir
-
-  test "greets the world" do
-    assert SimpleHttpServerWithElixir.hello() == :world
-  end
-end
